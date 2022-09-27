d40adc43bf1f47ff3f355fccbb8b67d07b780840
Atefeh Mohammaddoust <atefe.dev@gmail.com>
2022-09-27 19:27:56 +0330

    -a

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -27,7 +27,7 @@
             <!-- Modal Close Button -->
             <div
               class="modal-close cursor-pointer z-50"
-              @click="isOpen = false"
+              @click="modalVisibility = false"
             >
               <i class="fas fa-times"></i>
             </div>
@@ -161,7 +161,10 @@ export default {
   name: "AppAuth",
   computed: {
     ...mapState(useModalStore, ["hiddenClass"]),
-    ...mapWritableState(useModalStore, ["isOpen"]),
+    // ...mapWritableState(useModalStore, ["isOpen"]),
+    ...mapWritableState(useModalStore, {
+      modalVisibility: "isOpen",
+    }),
   },
 };
 </script>
