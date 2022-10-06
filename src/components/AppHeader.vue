fd9e17b68c2f8c182d91af9b48b254df6612c1cc
Atefeh Mohammaddoust <atefe.dev@gmail.com>
2022-10-06 13:59:09 +0330

    -a

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,9 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
+        >Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
@@ -23,7 +25,12 @@
               >
             </li>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="/manage"
+                >Manage</router-link
+              >
+              <router-link class="px-2 text-white" to="/about"
+                >About</router-link
+              >
             </li>
           </template>
         </ul>
